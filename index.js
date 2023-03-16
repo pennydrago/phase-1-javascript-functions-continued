043e396f21f7f0bb3138f5f7852509856dda2fbb
Penelope Drago <prd@drago.com.au>
2023-03-16 11:05:43 +1100

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+// Function called saturdayFun
+function saturdayFun (activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+
+// Function called mondayWork
+const mondayWork = function (workActivity="go to the office") {
+  return `This Monday, I will ${workActivity}.`;
+}
+
+// Function called wrapAdjective
+function wrapAdjective (flair="*") {
+  return function (word="special") {
+    return `You are ${flair}${word}${flair}!`;
+  }
+}
